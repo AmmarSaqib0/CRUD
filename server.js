f90297d7bcb0230e072897dc7cb123362888bc9c
ammarsaqib <ammarr.saqib@gmail.com>
2023-11-08 22:50:32 -0800

    got data from new employee form

--- server.js
+++ server.js
@@ -4,26 +4,39 @@ const app = express();
 const mssql = require("mssql");
 const dotenv = require('dotenv');
 dotenv.config();
+// Config your database credential
+const config = {
+    user: process.env.user,
+    password: process.env.password,
+    server: process.env.server,
+    database: process.env.database,
+    trustServerCertificate: true
+};
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+// app.use(express.json())
 
 app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
     res.render('addData.ejs');
+})
+
+app.post('/', function (req, res, err) {
+    if (err) console.log(err);
+    console.log(req.body.empId);
+    mssql.connect(config, function (err) {
+        let request = new mssql.Request();
+        request.query('create new',
+            function (err) {
+                if (err) console.log(err)
+            });
+    });
+    res.redirect(`/`);
 });
 
 // Get request
 app.get('/view', function (req, res) {
-    // Config your database credential
-    const config = {
-        user: process.env.user,
-        password: process.env.password,
-        server: process.env.server,
-        database: process.env.database,
-        trustServerCertificate: true
-    };
 
     // Connect to your database
     mssql.connect(config, function (err) {
@@ -31,7 +44,6 @@ app.get('/view', function (req, res) {
         request.query('select * from employees',
             function (err, records) {
                 if (err) console.log(err)
-                console.log(records.recordset.length);
                 res.render("viewdata.ejs", { records: records.recordset });
             });
     });
